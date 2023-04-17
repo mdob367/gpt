5dbe0353c690759bb19c31648042764781a1a49e
Matt Dobrin <mattdobrin@gmail.com>
2023-04-16 17:52:20 -0700

    add training and toy self attention

--- chatGPT.py
+++ chatGPT.py
@@ -1,6 +1,19 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import matplotlib.pyplot as plt
+
+
+# hyperparameters
+batch_size = 32 # How many independent samples to process at once
+block_size = 8 # Maximum context length for predictions
+epochs = 10000 # How many times to iterate over the entire dataset
+eval_interval = 300
+lr = 1e-3 # Learning rate
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Using device:', device)
+eval_iters = 100
+
 torch.manual_seed(1337)
 
 
@@ -14,12 +27,12 @@ class BigramLanguageModel(nn.Module):
     def forward(self, idx, targets=None):
 
         # idx and targets are both (B, T) tensor of integers
-        logits = self.token_embedding_table(idx) # (B, T, C)
+        logits = self.token_embedding_table(idx) # (B, T, C) Batch, Time, Channel
         
         if targets is None:
             loss = None
         else:
-            B, T, C = logits.shape
+            B, T, C = logits.shape # (B, T, C) Batch, Time, Channel
             logits = logits.view(B*T, C)
             targets = targets.view(B*T)
 
@@ -41,6 +54,7 @@ class BigramLanguageModel(nn.Module):
             # append sampled index to the running sequence
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
+    
 
 
 def load_shakespeare():
@@ -71,11 +85,30 @@ def get_batch(data, block_size, batch_size):
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
     return x, y
 
+@torch.no_grad()
+def estimate_losses():
+    losses = {}
+    m.eval() # Set model to evaluation mode
+    for split in ['train', 'val']:
+        if split == 'train':
+            data = trn_data
+        elif split == 'val':
+            data = val_data
+        losses[split] = 0
+        for _ in range(eval_iters):
+            X_batch, Y_batch = get_batch(data, block_size = block_size, batch_size = batch_size)
+            logits, loss = m(X_batch, Y_batch)
+            losses[split] += loss.item()
+        losses[split] /= eval_iters
+    m.train() # Set model back to training mode
+    return losses
 
 
-if __name__ == '__main__':
+def run_model():
+
     txt = load_shakespeare()
     encode, decode, vocab_size = create_encoder_decoder(txt)
     data = torch.tensor(encode(txt), dtype=torch.long)
@@ -89,9 +122,38 @@ if __name__ == '__main__':
     X_batch, Y_batch = get_batch(data, block_size = 8, batch_size = 4)
 
     m = BigramLanguageModel(vocab_size)
+    m = m.to(device)
     logits, loss = m(X_batch, Y_batch)
 
-    print(logits.shape)
-    print(loss)
+    optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
+    loss_history = []
+    for epoch in range(epochs):
+        X_batch, Y_batch = get_batch(data, block_size = 8, batch_size = batch_size)
+        logits, loss = m(X_batch, Y_batch)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+        loss_history.append(loss.item())
+        if epoch % eval_interval == 0:
+            losses = estimate_losses()
+            print(f"Epoch {epoch}: Train Loss: {losses['train']:.4f}, Val Loss: {losses['val']:.4f}")
+    plt.plot(loss_history)
+    plt.show()
+    print(decode(m.generate(idx=torch.zeros((1,1), dtype=torch.long), max_new_tokens=1000)[0].tolist()))
 
-    print(decode(m.generate(idx=torch.zeros((1,1), dtype=torch.long), max_new_tokens=100)[0].tolist()))
+if __name__ == '__main__':
+    # Toy example of self attention
+    B,T,C=4,8,2 # (B, T, C) Batch, Time, Channel
+    x = torch.randn(B,T,C)
+    print(x.shape)
+
+    # Want x[b,t] to be mean for i<=t of x[b,i]
+    x_bow = torch.zeros(B,T,C)
+    for b in range(B):
+        for t in range(T):
+            xprev = x[b, :t+1, :] # (t, C)
+            x_bow[b,t] = torch.mean(xprev, dim=0)
+    print(x[0])
+    print(x_bow[:,-1])
+    print(x_bow.shape)
+    # run_model()
